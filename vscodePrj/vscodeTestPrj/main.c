269e70d67e16961a34715983241acf2c0eeead85
Vignesh R <Vignesh.R@microchip.com>
2026-04-21 12:23:51 +0530

    Testing CITD-597

---
+++ vscodePrj/vscodeTestPrj/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author I78279
+ * @date 2026-02-16
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
